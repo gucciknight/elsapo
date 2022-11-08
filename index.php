295061921bab6cbd6767abb6915bfeda316c3b99
gucciknight <arodriguezl3>
2022-11-08 00:20:29 -0300

    elsapolio

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
-<? php
-$postdata = array(
-    'name' => 'Arfan'
-);
+<?php
 
 $url = "https://api.demo.sitehost.co.nz/1.0/srs/list_domains.json?client_id=293785&apikey=d17261d51ff7046b760bd95b4ce781ac";
 
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_HEADER, false);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata);
-
-$json_response = curl_exec($curl);
-$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-curl_close($curl);
+$json = file_get_contents( $url );
+$arr = json_decode( $json, TRUE );
+foreach ( $arr AS $element ) {
+    foreach ( $element AS $e ) {
+      echo $e['DeviceName'] . '<br>';
+    }
+  }
 ?>
